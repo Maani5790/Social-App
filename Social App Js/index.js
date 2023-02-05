d4aac7e08cb7f4f7c1a4c84efd2466bbd18be722
Abdul Rehman <114314272+Maani5790@users.noreply.github.com>
2023-02-05 17:13:18 +0500

    Add files via upload

---
+++ Social App Js/index.js
@@ -0,0 +1,77 @@
+// object
+
+
+// let bios = {
+//     name : "rehman",
+//     age : 23,
+//     city : "karachi"
+// }
+
+// console.log(bios);
+// console.log(this.bios);
+// bios.ft = "ku"
+
+// console.log(bios);
+
+// bios.age = 24;
+// console.log(bios);
+
+// let converts = JSON.stringify(bios);
+// let convo = JSON.parse(converts)
+// console.log(bios);
+// console.log(convo);
+// console.log(converts);
+
+
+////////////////////////// Dom //////////////////
+
+
+// let num1 = document.getElementById("one");
+// console.log(num1.textContent);
+// console.log(num1.text);
+// console.log(num1.tagName);
+// console.log(num1.innerHTML);
+// let action = document.getElementById('btn').textContent = "welcome"
+// let action1 = document.getElementsByClassName('para')[0].textContent = "nothing"
+// let btn1  =document.getElementById('btn');
+// btn.style.background = "black";
+
+window.open();
+Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+window.open("www.facebook.com");
+Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+window.open("http://www.facebook.com/");
+Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+window.open("", "alert", "width=400, height=400");
+Window {window: Window, self: Window, document: document, name: 'alert', location: Location, …}
+window.location
+Location {ancestorOrigins: DOMStringList, href: 'chrome://newtab/', origin: 'chrome://newtab', protocol: 'chrome:', host: 'newtab', …}
+window.location.hostname
+'newtab'
+window.location.protocol
+'chrome:'
+window.location.pathname
+'/'
+window.assign(www.facebook.com)
+VM781:1 Uncaught ReferenceError: www is not defined
+    at <anonymous>:1:15
+(anonymous) @ VM781:1
+window.assign("www.facebook.com")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
